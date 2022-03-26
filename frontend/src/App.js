0e436049ac28f2a17ce8c1369aaeb0c09b464cd7
Anas <alnabale2016@gmail.com>
2022-03-26 20:42:54 +0300

    login + failed popup + login reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./components/auth/signup";
 import Navigation from './components/navigation';
-
+import Login from './components/auth/login';
 const App = () => {
 	return <div className="App">
 		<Navigation/>
 		{/* <SignUp/> */}
 		<Routes>
+			<Route path ='/login' element={<Login/>}/>
 			<Route path ='/signup' element={<SignUp/>}/>
 		</Routes>
 	</div>;
---
+++ frontend/src/components/auth/login.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+import { setToken, setStatus } from "../../reducers/login";
+import CustomPopup from "../popup";
+
+
+const Login = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [toggleConfirmButton, setToggleConfirmButton] = useState(true);
+    const [visibility, setVisibility] = useState(false);
+
+
+    const openPopUb = (sellerId, sellerName) => {
+        //re-setState
+        if (toggleConfirmButton === false) setToggleConfirmButton(true);
+        
+        //open pop-up
+        setVisibility(!visibility);
+    };
+
+    const popupCloseHandler = (e) => {
+        setVisibility(e);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:8000/login", { email, password })
+            .then((result) => {
+                const user = jwt_decode(result.data["token"]);
+                if (result) {
+                    dispatch(setToken({ token: result.data["token"], user }));
+
+                    localStorage.setItem("token", result.data["token"]);
+                    localStorage.setItem("id", user.userId);
+
+                    // setMessage("The user has been loggedIn successfully ");
+
+                    if (user.type === "user") {
+                        navigate("/");
+                        dispatch(setStatus(true));
+                    } else {
+                        // If we had another type of clients we can navigate here.
+                    }
+                } else {
+                    setMessage("Error happened while login, please try again");
+                    openPopUb()
+                    setTimeout(() => {
+                        setMessage("");
+                    }, 3000);
+                }
+            })
+            .catch((err) => {
+                setMessage("Password or Email is incorrect");
+                openPopUb()
+            })
+            
+    };
+
+    return (
+        <main className="login-body fixed">
+            <div className="login-box">
+                <h2 className="login-top-text">LOGIN</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="login-input-box">
+                        <label className="login-label">Email</label>
+                        <br />
+                        <input
+                            className="login-input"
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="login-input-box">
+                        <label className="login-label">Password</label>
+                        <br />
+                        <input
+                            className="login-input"
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button onSubmit={handleSubmit} className="login-submit-btn">
+                        SUBMIT
+                    </button>
+                    <div className="divider"></div>
+
+                    {/* Avoiding extra white space*/}
+                    <CustomPopup
+                                onClose={popupCloseHandler}
+                                show={visibility}
+                                title="Warning"
+                            >
+                                {message}
+                            </CustomPopup>
+                    {/* <p className={message === "" ? "" : "form-message"}>{message} </p> */}
+                    <p className="footer-login-text">
+                        don't have an account?
+                        <span
+                            className="switch-link"
+                            onClick={() => {
+                                navigate("/signup");
+                            }}
+                        >
+                            Create Account here
+                        </span>
+                    </p>
+                </form>
+            </div>
+        </main>
+    );
+};
+
+export default Login;
